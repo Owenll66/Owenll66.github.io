4a9a02fe0d0a100bc65afcec1a5110d84111233c
bowen <592623044@qq.com>
2019-06-23 22:29:19 +0930

    update

--- flappyBirdResource/flappyBird.js
+++ flappyBirdResource/flappyBird.js
@@ -78,7 +78,7 @@ function draw(){
         // detect collision
         
         if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
-            window.alert("Your score: " + score)
+            window.alert("Your score: " + score);
             location.reload(); // reload the page
         }
         
